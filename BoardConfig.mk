bc367945f8b86325bd14c8a26f5dec4e5f27c71c
Hasan <hasan317@proton.me>
2024-11-06 16:53:33 +0700

    whyred: Use legacy battery services

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 4095
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_THEME := portrait_hdpi
+TW_USE_LEGACY_BATTERY_SERVICES := true
 TW_USE_TOOLBOX := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
